8389526e5c95206b44fb6ddc7d3075b1648c5dcf
tessatiu <gadjiew.rasim@gmail.com>
2023-10-01 09:28:55 +0300

    udplistener

---
+++ cmake-build-debug/CMakeFiles/udplistener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/udplistener.dir/main.cpp.o"
+  "udplistener"
+  "udplistener.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/udplistener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
